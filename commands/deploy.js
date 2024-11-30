0418974b549252bb0767a16f7999e9f7d155bac7
Kamran Mohammed <kamranabdulbasith@gmail.com>
2024-11-30 16:55:42 +0530

    changed the commands list

--- commands/deploy.js
+++ commands/deploy.js
@@ -5,15 +5,18 @@ module.exports = async function deploy() {
   console.log(chalk.blue("Deploying API documentation..."));
 
   const commands = `
-    git fetch origin gh-pages &&
-    git checkout gh-pages || git checkout --orphan gh-pages &&
-    git reset --hard &&
-    git clean -fd &&
-    git checkout main &&
-    git subtree split --prefix docs -b gh-pages-temp &&
-    git push origin gh-pages-temp:gh-pages -f &&
-    git branch -D gh-pages-temp
-  `;
+  git fetch origin gh-pages ||
+  git checkout --orphan gh-pages &&
+  git reset --hard &&
+  git clean -fd &&
+  git commit --allow-empty -m "Initialize gh-pages branch" &&
+  git push origin gh-pages &&
+  git checkout main &&
+  git subtree split --prefix docs -b gh-pages-temp &&
+  git push origin gh-pages-temp:gh-pages -f &&
+  git branch -D gh-pages-temp
+`;
+
   // Push the "docs" folder to the `gh-pages` branch using Git subtree
   exec(commands, (err, stdout, stderr) => {
     if (err) {
